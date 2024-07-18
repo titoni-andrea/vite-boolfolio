e6bba3fabbe2fba9b8b6bb336fde7b6e795bbec5
titoni-andrea <andreatitoni@gmail.com>
2024-07-18 15:33:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script >
+import axios from 'axios';
+
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      projects: ''
+    }
+  },
+
+mounted() {
+  
+axios
+.get('http://127.0.0.1:8000/api/projects')
+.then(response => {
+  console.log(response);
+  this.projects = response.data.projects
+  
+
+
+})
+
+},
+
+
+
+
+}
+</script>
+
+<template>
+  <h1>Welcomee</h1>
+
+</template>
+
+<style scoped>
+
+</style>
